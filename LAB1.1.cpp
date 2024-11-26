b8aebf52195e84a9cd3079269c25b47037a74869
6704062611352 <s6704062611352@email.kmutnb.ac.th>
2024-11-26 14:57:04 +0700

    Add files via upload

---
+++ LAB1.1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() 
+{
+    int var1 = 100;
+    int var2 = 10;
+    if(var1 > var2) {
+	    printf("var 1 is greater than var 2");
+    } else {
+        printf("var 2 is greater than var 1");
+    }
+    return 0;
+}
