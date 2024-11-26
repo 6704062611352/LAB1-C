58faf6469eacf72e0fabb9bd475dc319a33b92b6
6704062611352 <s6704062611352@email.kmutnb.ac.th>
2024-11-26 15:00:45 +0700

    Add files via upload

--- LAB1.1.cpp
+++ LAB1.1.cpp
@@ -1,12 +1,16 @@
 #include<stdio.h>
 int main() 
 {
-    int var1 = 100;
-    int var2 = 10;
+    int var1 =0;
+    int var2 =0;
+    scanf("%d" ,& var1);
+    scanf("%d" ,& var2);
     if(var1 > var2) {
 	    printf("var 1 is greater than var 2");
+        printf("\n%d",(var1-var2));
     } else {
         printf("var 2 is greater than var 1");
+        printf("\n%d",(var2-var1));
     }
     return 0;
 }
